363094b8e2cc84a5583c7a6430f2215fc2f74995
CornelZ <zgardancornel@gmail.com>
2022-02-28 12:26:40 +0200

    removed clutter

--- test/test/FooTests.java
+++ test/test/FooTests.java
@@ -3,6 +3,7 @@ package test;
 import static org.junit.Assert.assertEquals;
 
 import controllers.routes;
+import controllers.ReverseFooController;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,13 @@ public class FooTests {
   public void test001() {
     var fooUrl = routes.FooController.index().url();
     LOG.debug("fooUrl: {}", fooUrl);
-    assertEquals("/v1/bar/foo", fooUrl);
+    assertEquals("/v1/bar/foo", fooUrl); // /foo fail
+  }
+
+  @Test
+  public void test002() {
+    var fooUrl = new ReverseFooController(() -> "/v1/bar").index().url();
+    LOG.debug("fooUrl: {}", fooUrl);
+    assertEquals("/v1/bar/foo", fooUrl); // ok
   }
 }
